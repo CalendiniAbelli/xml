cc3a8ff999d7c72d7a5e580e357ecf2dc07e002c
NaudEric <eric.naud@etu.unice.fr>
2023-02-11 12:04:58 +0100

    tuto

--- Organisme/src/Main.java
+++ Organisme/src/Main.java
@@ -14,6 +14,8 @@ import org.dom4j.io.SAXReader;
  * Liste l'ensemble des voyages qui ne sont pas encore déroulés mais ou la capacité de client inscrit est deja maximale.
  * Pour ceci, il faut verifier que la somme du nombre de client inscrit pour tous les voyages ayant la même offre à la même date
  * soit égale à la capacité de l'offre.
+ *
+ * Pour utiliser ce fichier, il faut ajouter l'API dom4J disponible à la source du projet dans les librairies
  */
 
 public class Main {
